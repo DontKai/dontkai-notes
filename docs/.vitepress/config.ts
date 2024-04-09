939d49f1c704f8ded80280e6ebb2bb5fa0df7492
邓凯 <dengkai@adas.com>
2024-04-09 15:42:02 +0800

    update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     // head 会被渲染成 <link .... >
     base: '/dontkai-notes/',
     head: [
-        ['link', { rel: 'icon', href: '/favicon.ico' }], // 页头icon
+        ['link', { rel: 'icon', href: '/dontkai-notes/hero.png' }], // 页头icon
         ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }] // 字体
     ],
     lang: 'zh-CN',
