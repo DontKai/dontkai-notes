071a58b324293fb97642d70edfc3e16e7f3a9d6d
邓凯 <dengkai@adas.com>
2024-04-10 14:07:33 +0800

    update

--- docs/.vitepress/router/routes/JavaScript.ts
+++ docs/.vitepress/router/routes/JavaScript.ts
@@ -1,18 +1,14 @@
 /*
  * @file: JavaScript 路由
  * @author: DontK
- * @LastEditTime: 2024-04-09 18:17:48
+ * @LastEditTime: 2024-04-10 14:05:01
  */
 export default [
     {
-        text: 'ES6 系列',
-        items: []
-    },
-    {
-        text: '深入系列',
+        text: '系列一',
         items: [
             {
-                text: '从原型到原型链',
+                text: '原型链',
                 link: '/page/frontend/JavaScript/201_Prototype'
             },
             {
@@ -20,13 +16,25 @@ export default [
                 link: '/page/frontend/JavaScript/202_Scope'
             },
             {
+                text: '执行上下文栈',
+                link: '/page/frontend/JavaScript/203_ExecutionContextStack'
+            },
+            {
                 text: '执行上下文',
-                link: '/page/frontend/JavaScript/203_ExecutionContext'
+                link: '/page/frontend/JavaScript/204_ExecutionContext'
+            },
+            {
+                text: '作用域链',
+                link: '/page/frontend/JavaScript/205_ScopeChain'
+            },
+            {
+                text: 'this',
+                link: '/page/frontend/JavaScript/206_This'
             }
         ]
     },
     {
-        text: '专题系列',
+        text: '系列二',
         items: [
             {
                 text: '栈-深拷贝和浅拷贝',
@@ -47,7 +55,7 @@ export default [
         ]
     },
     {
-        text: '面试系列',
+        text: '系列三',
         items: []
     }
 ]
