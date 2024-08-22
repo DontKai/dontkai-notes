ac13684ab971c414ba96860ca07a5cd9c1f7fb8e
邓凯 <dengkai@adas.com>
2024-08-22 15:17:40 +0800

    feats：新增utils-math内容

--- docs/utils/utils-math.ts
+++ docs/utils/utils-math.ts
@@ -1,7 +1,7 @@
 /*
  * @file: 计算相关工具
  * @author: DontK
- * @LastEditTime: 2024-08-14 14:41:04
+ * @LastEditTime: 2024-08-22 15:16:28
  */
 
 /**
@@ -10,7 +10,7 @@
  * @param max 最大整数
  * @return 随机数
  */
-export const getRandomNumber = (min: number, max: number) => {
+export const getRandomNumber = (min: number, max: number): number => {
     // 计算范围内的随机数
     let random = Math.random() * (max - min + 1) + min
     // 向下取整得到整数
@@ -33,7 +33,7 @@ export const flattenTree = (tree: any[], key?: string): any[] => {
         } else {
             result.push(node)
         }
-        if (node.children && node.children.length) {
+        if (Array.isArray(node.children) && node.children.length) {
             result.push(...flattenTree(node.children, key))
         }
     })
@@ -46,10 +46,10 @@ export const flattenTree = (tree: any[], key?: string): any[] => {
  * @param key 取出树结构最后一级子级某个属性
  * @return 一维数组
  */
-export const flattenTreeLast = (tree: any[], key: string) => {
+export const flattenTreeLast = (tree: any[], key: string): any[] => {
     const result: any = []
     tree.forEach((node: any) => {
-        if (node.children && node.children.length) {
+        if (Array.isArray(node.children) && node.children.length) {
             result.push(...flattenTreeLast(node.children, key))
         } else if (key) {
             result.push(node[key])
@@ -58,4 +58,40 @@ export const flattenTreeLast = (tree: any[], key: string) => {
         }
     })
     return result
+}
+
+/**
+ * @description: 数组转为树结构
+ * @param array 原数组
+ * @param parentId 匹配属性
+ * @return 树结构数组
+ */
+export const arrayToTree = (array: any[], parentId?: any): any[] => {
+    const results: any[] = []
+    if (Array.isArray(array) && array.length) {
+        array.forEach((item: any) => {
+            if (item.parentId === parentId) {
+                results.push({
+                    ...item,
+                    children: arrayToTree(array, item.id)
+                })
+            }
+        })
+    }
+    return results
+}
+
+/**
+ * @description: 将多层嵌套的数组扁平化为单层数组。
+ * @param arr 包含元素或子数组的数组。
+ * @return 扁平化后的单层数组，其中包含所有嵌套元素。
+ */
+export const flattenArray = (arr: any[]): any[] => {
+    return arr.reduce((acc, item) => {
+        if (Array.isArray(item)) {
+            return acc.concat(flattenArray(item))
+        } else {
+            return acc.concat(item)
+        }
+    }, [])
 }
