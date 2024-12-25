0d02cb92a65828ad40cb5d98c849597e1dcabf71
邓凯 <dengkai@adas.com>
2024-12-25 09:26:22 +0800

    feat：新增文件

--- docs/.vitepress/router/routes/CSS.ts
+++ docs/.vitepress/router/routes/CSS.ts
@@ -1,23 +1,31 @@
 /*
  * @file: CSS 路由
  * @author: DontK
- * @LastEditTime: 2024-12-24 13:49:05
+ * @LastEditTime: 2024-12-24 16:47:31
  */
-export const CSS_Nav = { text: 'CSS', link: '/page/CSS/newFeatures/' }
+export const CSS_Nav = { text: 'CSS', link: '/page/CSS/newFeatures' }
 
 export default {
     '/page/CSS/': [
         {
             text: 'CSS3 新特性',
-            link: '/page/CSS/newFeatures2/'
+            link: '/page/CSS/newFeatures'
         },
         {
-            text: 'CSS 单位',
-            link: '/page/CSS/pixel/'
+            text: 'CSS3 单位',
+            link: '/page/CSS/pixel'
         },
         {
-            text: '重排和重绘（回流）',
-            link: '/page/CSS/reflowRedraw/'
+            text: 'CSS3 通用1',
+            link: '/page/CSS/common1'
+        },
+        {
+            text: 'CSS3 通用2',
+            link: '/page/CSS/common2'
+        },
+        {
+            text: 'CSS3 重排和重绘（回流）',
+            link: '/page/CSS/reflowRedraw'
         }
     ]
 }
---
+++ docs/page/CSS/common2.md
@@ -0,0 +1,12 @@
+# CSS3 通用 2
+
+## 六、说说你对 BFC 的理解，如何创建 BFC？
+
+BFC（块级格式化上下文，Block Formatting Context）是 CSS 中一种非常重要的布局概念，是一个独立的渲染区域，使其内部的布局不受外部元素影响。了解 BFC 有助于解决浮动、清除浮动、边距重叠等常见布局问题。
+
+创建 BFC 非常简单，常见的方法有：
+
+1. 设置元素的 float 属性为 left 或 right。
+2. 设置元素的 position 属性为 absolute 或 fixed。
+3. 设置元素的 display 属性为 inline-block 或 table-cell 等。
+4. 设置元素的 overflow 属性为 hidden、auto 或 scroll。
