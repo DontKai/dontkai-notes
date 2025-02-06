06d2fda86e2c73b3c74bce0251fef32e70e63855
邓凯 <1403698978@qq.com>
2025-02-06 14:10:31 +0800

    fix: 整理内容

--- docs/.vitepress/router/navConf.ts
+++ docs/.vitepress/router/navConf.ts
@@ -3,7 +3,6 @@ import { CSS_Nav } from './routes/CSS'
 import { JavaScript_Nav } from './routes/JavaScript'
 import { TypeScript_Nav } from './routes/TypeScript'
 import { Vue_Nav } from './routes/Vue'
-import { Components_Nav } from './routes/Components'
 import { Plugins_Nav } from './routes/Plugins'
 import { Others_Nav } from './routes/Others'
 
@@ -11,6 +10,6 @@ export default [
     { text: '主页', link: '/' },
     {
         text: '前端',
-        items: [HTML_Nav, CSS_Nav, JavaScript_Nav, TypeScript_Nav, Vue_Nav, Components_Nav, Plugins_Nav, Others_Nav]
+        items: [HTML_Nav, CSS_Nav, JavaScript_Nav, TypeScript_Nav, Vue_Nav, Plugins_Nav, Others_Nav]
     }
 ]
--- docs/.vitepress/router/sidebarConf.ts
+++ docs/.vitepress/router/sidebarConf.ts
@@ -1,7 +1,7 @@
 /*
  * @file: 侧边栏导航
  * @author: DontK
- * @LastEditTime: 2025-01-16 16:47:56
+ * @LastEditTime: 2025-02-06 14:04:50
  */
 import { DefaultTheme } from 'vitepress'
 import HTML from './routes/HTML'
@@ -9,7 +9,6 @@ import CSS from './routes/CSS'
 import JavaScript from './routes/JavaScript'
 import TypeScript from './routes/TypeScript'
 import Vue from './routes/Vue'
-import Components from './routes/Components'
 import Plugins from './routes/Plugins'
 import Others from './routes/Others'
 
@@ -19,7 +18,6 @@ const sidebarConf: DefaultTheme.Sidebar = {
     ...JavaScript,
     ...TypeScript,
     ...Vue,
-    ...Components,
     ...Plugins,
     ...Others
 }
